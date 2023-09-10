dc69e553fc126d051a264568751af6af2797f739
brunolcarli <brunolcarli@gmail.com>
2023-09-10 03:33:30 -0300

    ADD store csv in database

--- battle_log/models.py
+++ battle_log/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class BattleLogCSV(models.Model):
+    data = models.TextField()
+    title = models.CharField(max_length=1000, null=False, blank=False)
